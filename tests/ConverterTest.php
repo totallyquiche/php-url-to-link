440a8ec5def86c0ad4d6c77425d7e671ec5bbaf1
Brian Dady <bndady@gmail.com>
2021-11-13 16:49:14 -0600

    Minor formatting changes

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -1,126 +1,121 @@
-<?php
-    declare(strict_types = 1);
+<?php declare(strict_types = 1);
 
-    namespace TotallyQuiche\URLtoLink;
+namespace TotallyQuiche\URLtoLink;
 
-    require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
-    use \PHPUnit\Framework\TestCase;
+use \PHPUnit\Framework\TestCase;
+
+class ConverterTest extends TestCase {
+    /**
+     * Provides test cases and expected results for parse method
+     *
+     * @return array
+     */
+    public function parseProvider() : array {
+        return [
+            'Empty string' => [
+                '',
+                []
+            ],
+            'Single URL' => [
+                'https://www.example.com:123/forum/questions/?tag=networking&order=newest#top',
+                [
+                    'https://www.example.com:123/forum/questions/?tag=networking&order=newest#top'
+                ]
+            ],
+            'Multiple URLs' => [
+                'https://www.google.com http://www.google.com',
+                [
+                    'https://www.google.com',
+                    'http://www.google.com'
+                ]
+            ],
+            'Duplicate URLs' => [
+                'https://www.google.com https://www.google.com',
+                [
+                    'https://www.google.com',
+                ]
+            ],
+            'URL with non-URL' => [
+                'https://www.google.com is a URL.',
+                [
+                    'https://www.google.com',
+                ]
+            ],
+            'Period in URL' => [
+                'https://www.google.com. is a URL.',
+                [
+                    'https://www.google.com.',
+                ]
+            ]
+        ];
+    }
 
     /**
-     * Tests Converter
+     * Tests parse method.
      *
-     * @group  Converter
-     * @covers \TotallyQuiche\URLtoLink\Converter
+     * @param string $string
+     * @param array  $expected_result
+     *
+     * @return void
      */
-    class ConverterTest extends TestCase {
-        /**
-         * Provides test cases and expected results for parse method
-         *
-         * @return array
-         */
-        public function parseProvider() : array {
-            return [
-                'Empty string' => [
-                    '',
-                    []
-                ],
-                'Single URL' => [
-                    'https://www.example.com:123/forum/questions/?tag=networking&order=newest#top',
-                    [
-                        'https://www.example.com:123/forum/questions/?tag=networking&order=newest#top'
-                    ]
-                ],
-                'Multiple URLs' => [
-                    'https://www.google.com http://www.google.com',
-                    [
-                        'https://www.google.com',
-                        'http://www.google.com'
-                    ]
-                ],
-                'Duplicate URLs' => [
-                    'https://www.google.com https://www.google.com',
-                    [
-                        'https://www.google.com',
-                    ]
-                ],
-                'URL with non-URL' => [
-                    'https://www.google.com is a URL.',
-                    [
-                        'https://www.google.com',
-                    ]
-                ],
-                'Period in URL' => [
-                    'https://www.google.com. is a URL.',
-                    [
-                        'https://www.google.com.',
-                    ]
-                ]
-            ];
-        }
+    public function testParse(string $string, array $expected_result) : void {
+        $converter = new Converter;
+        $result = $converter->parse($string);
 
-        /**
-         * Tests parse method
-         *
-         * @dataProvider parseProvider
-         * @param        string        $string
-         * @param        array         $expected_result
-         * @return       void
-         */
-        public function testParse(string $string, array $expected_result) : void {
-            $converter = new Converter;
-            $result = $converter->parse($string);
-
-            $this->assertEquals($expected_result, $result);
-        }
-
-        /**
-         * Provides test cases and expected results for convert method
-         *
-         * @return array
-         */
-        public function convertProvider() : array {
-            return [
-                'Empty string' => [
-                    '',
-                    ''
-                ],
-                'Single URL' => [
-                    'https://www.example.com:123/forum/questions/?tag=networking&order=newest#top',
-                    '<a href="https://www.example.com:123/forum/questions/?tag=networking&order=newest#top" target="_SELF">https://www.example.com:123/forum/questions/?tag=networking&order=newest#top</a>'
-                ],
-                'Multiple URLs' => [
-                    'https://www.google.com http://www.google.com',
-                    '<a href="https://www.google.com" target="_SELF">https://www.google.com</a> <a href="http://www.google.com" target="_SELF">http://www.google.com</a>'
-                ],
-                'Duplicate URLs' => [
-                    'https://www.google.com https://www.google.com',
-                    '<a href="https://www.google.com" target="_SELF">https://www.google.com</a> <a href="https://www.google.com" target="_SELF">https://www.google.com</a>'
-
-                ],
-                'URL with non-URL' => [
-                    'https://www.google.com is a URL.',
-                    '<a href="https://www.google.com" target="_SELF">https://www.google.com</a> is a URL.'
-                ],
-                'Period in URL' => [
-                    'https://www.google.com. is a URL.',
-                    '<a href="https://www.google.com." target="_SELF">https://www.google.com.</a> is a URL.'
-                ],
-            ];
-        }
-
-        /**
-         * Tests convert method
-         *
-         * @dataProvider convertProvider
-         * @param        string          $string
-         * @param        string          $expected_result
-         * @return       void
-         */
-        public function testConvert(string $string, string $expected_result) : void {
-            $converter = new Converter;
-            $result = $converter->convert($string);
-
-            $this->assertEquals($expected_result, $result);
-        }
+        $this->assertEquals($expected_result, $result);
     }
+
+    /**
+     * Provides test cases and expected results for convert method.
+     *
+     * @return array
+     */
+    public function convertProvider() : array {
+        return [
+            'Empty string' => [
+                '',
+                ''
+            ],
+            'Single URL' => [
+                'https://www.example.com:123/forum/questions/?tag=networking&order=newest#top',
+                '<a href="https://www.example.com:123/forum/questions/?tag=networking&order=newest#top" target="_SELF">https://www.example.com:123/forum/questions/?tag=networking&order=newest#top</a>'
+            ],
+            'Multiple URLs' => [
+                'https://www.google.com http://www.google.com',
+                '<a href="https://www.google.com" target="_SELF">https://www.google.com</a> <a href="http://www.google.com" target="_SELF">http://www.google.com</a>'
+            ],
+            'Duplicate URLs' => [
+                'https://www.google.com https://www.google.com',
+                '<a href="https://www.google.com" target="_SELF">https://www.google.com</a> <a href="https://www.google.com" target="_SELF">https://www.google.com</a>'
+
+            ],
+            'URL with non-URL' => [
+                'https://www.google.com is a URL.',
+                '<a href="https://www.google.com" target="_SELF">https://www.google.com</a> is a URL.'
+            ],
+            'Period in URL' => [
+                'https://www.google.com. is a URL.',
+                '<a href="https://www.google.com." target="_SELF">https://www.google.com.</a> is a URL.'
+            ],
+        ];
+    }
+
+    /**
+     * Tests convert method.
+     *
+     * @dataProvider convertProvider
+     *
+     * @param string $string
+     * @param string $expected_result
+     *
+     * @return void
+     */
+    public function testConvert(string $string, string $expected_result) : void {
+        $converter = new Converter;
+        $result = $converter->convert($string);
+
+        $this->assertEquals($expected_result, $result);
+    }
+}
