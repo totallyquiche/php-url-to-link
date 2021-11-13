5658b4cfefb5fc96e29dd7ca730f19911a6f3e43
Brian Dady <bndady@gmail.com>
2021-11-13 17:00:07 -0600

    Minor formatting and docblock additions

--- src/Converter.php
+++ src/Converter.php
@@ -1,67 +1,112 @@
-<?php
-    declare(strict_types = 1);
+<?php declare(strict_types = 1);
 
-    namespace TotallyQuiche\URLtoLink;
+namespace TotallyQuiche\URLtoLink;
 
-    class Converter {
-        const SCHEME_REGEX = '(?:(?:\b)(?:http|https)\:\/\/)';
-        const HOST_REGEX = '(?:[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\.\-\_]*)';
-        const PORT_REGEX = '(?:(?:\:[0-9]+))';
-        const PATH_REGEX = '(?:(?:\/[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\:\@\/\-\_\.]*)?)';
-        const QUERY_REGEX = '(?:\?[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\:\@\/\-\_\.]*)';
-        const FRAGMENT_REGEX = '(?:\#[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\:\@\/\-\_\.]*)';
+class Converter {
+    /**
+     * @const string
+     */
+    private const SCHEME_REGEX = '(?:(?:\b)(?:http|https)\:\/\/)';
 
-        private $url_regex;
+    /**
+     * @const string
+     */
+    const HOST_REGEX = '(?:[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\.\-\_]*)';
 
-        /**
-         * Initializes this object
-         */
-        public function __construct() {
-            $this->url_regex = '/(?<URL>' . self::SCHEME_REGEX . self::HOST_REGEX . self::PORT_REGEX . '?' . self::PATH_REGEX . '?' . self::QUERY_REGEX . '?' . self::FRAGMENT_REGEX . '?)/';
-        }
+    /**
+     * @const string
+     */
+    const PORT_REGEX = '(?:(?:\:[0-9]+))';
 
-        /**
-         * Accepts a string and returns an array of unique URLs found therein
-         *
-         * @param  string $string
-         * @return array
-         */
-        public function parse(string $string) : array {
-            preg_match_all($this->url_regex,
-                           $string,
-                           $matches,
-                           PREG_SET_ORDER);
+    /**
+     * @const string
+     */
+    const PATH_REGEX = '(?:(?:\/[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\:\@\/\-\_\.]*)?)';
 
-            $urls = [];
+    /**
+     * @const string
+     */
+    const QUERY_REGEX = '(?:\?[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\:\@\/\-\_\.]*)';
 
-            foreach ($matches as $match) {
-                $urls[] = $match['URL'];
-            }
+    /**
+     * @const string
+     */
+    const FRAGMENT_REGEX = '(?:\#[a-zA-Z0-9\!\$\&\'\(\)\*\+\,\;\=\%\:\@\/\-\_\.]*)';
 
-            return array_unique($urls);
-        }
+    /**
+     * @var string
+     */
+    private $url_regex;
 
-        /**
-         * Accepts a string, converts URLs found therein into HTML anchor tags, and returns the resulting string
-         *
-         * @param  string $string
-         * @param  string $target
-         * @return string
-         */
-        public function convert(string $string, string $target = '_SELF') : string {
-            $urls = $this->parse($string);
-
-            // Sort URLs so we don't make changes to longer URLs that contain short URLs
-            usort($urls, function($first, $second) {
-                return strlen($first) - strlen($second);
-            });
-
-            foreach ($urls as $url) {
-                $string = str_replace($url,
-                                      '<a href="' . $url . '" target="' . $target . '">' . $url . '</a>',
-                                      $string);
-            }
-
-            return $string;
-        }
+    /**
+     * Initializes this object.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->url_regex = '/(?<URL>' .
+            self::SCHEME_REGEX .
+            self::HOST_REGEX .
+            self::PORT_REGEX .
+            '?' .
+            self::PATH_REGEX .
+            '?' .
+            self::QUERY_REGEX .
+            '?' .
+            self::FRAGMENT_REGEX .
+            '?)/';
     }
+
+    /**
+     * Accepts a string and returns an array of unique URLs found therein
+     *
+     * @param string $string
+     *
+     * @return array
+     */
+    public function parse(string $string) : array
+    {
+        preg_match_all(
+            $this->url_regex,
+            $string,
+            $matches,
+            PREG_SET_ORDER
+        );
+
+        $urls = [];
+
+        foreach ($matches as $match) {
+            $urls[] = $match['URL'];
+        }
+
+        return array_unique($urls);
+    }
+
+    /**
+     * Accepts a string, converts URLs found therein into HTML anchor tags, and
+     * returns the resulting string.
+     *
+     * @param string $string
+     * @param string $target
+     *
+     * @return string
+     */
+    public function convert(string $string, string $target = '_SELF') : string
+    {
+        $urls = $this->parse($string);
+
+        // Sort URLs so we don't make changes to longer URLs that contain short URLs
+        usort($urls, fn($a, $b) => strlen($a) - strlen($b));
+
+        foreach ($urls as $url) {
+            $string = str_replace(
+                $url,
+                '<a href="' . $url . '" target="' . $target . '">' . $url . '</a>',
+                $string
+            );
+        }
+
+        return $string;
+    }
+}
